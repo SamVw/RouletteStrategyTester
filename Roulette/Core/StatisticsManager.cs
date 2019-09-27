3b36c6b56c93f3a525a1e49fd6cb8917ed554151
Sam <sam.vanwelsenaere@cegeka.com>
2019-09-27 08:35:32 +0200

    Automapper integration

--- Roulette/Core/StatisticsManager.cs
+++ Roulette/Core/StatisticsManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using Roulette.Core.Game.Bets;
 using Roulette.Core.Interfaces;
 using Roulette.Core.Models;
@@ -12,42 +13,28 @@ namespace Roulette.Core
     public class StatisticsManager : IStatisticsManager
     {
         private readonly Dictionary<string, StrategyStatistics> _strategyResults;
+        private IMapper _mapper;
 
         public StatisticsManager()
         {
             _strategyResults = new Dictionary<string, StrategyStatistics>();
+
+            var config = MapperConfiguration();
+            _mapper = new Mapper(config);
         }
 
         public void Process(StrategyResult result)
         {
-            var statistic = new StrategyStatistics()
-            {
-                Cycles = result.CyclesRan,
-                Strategy = result.Strategy,
-                EndBudget = result.EndBudget,
-                MaxBet = result.MaxBet,
-                MinBet = result.MinBet,
-                Average = result.AllBets.Average(),
-                Median = GetMedian(result.AllBets),
-                StartBudget = result.StartBudget,
-                EndBalance = result.EndBudget - result.StartBudget,
-                Name = result.Name,
-                MinBudget = result.MinBudget,
-                MaxBudget = result.MaxBudget
-            };
+            var statistic = _mapper.Map<StrategyStatistics>(result);
 
             if (!_strategyResults.ContainsKey(result.Strategy))
             {
-                _strategyResults.Add(result.Strategy, new StrategyStatistics());
+                _strategyResults.Add(result.Strategy, statistic);
             }
-
-            _strategyResults[result.Strategy] = statistic;
-        }
-
-        private double GetMedian(List<double> bets)
-        {
-            var orderedBets = bets.OrderBy(b => b).ToList();
-            return orderedBets[(int)(orderedBets.Count / 2)];
+            else
+            {
+                _strategyResults[result.Strategy] = statistic;
+            }
         }
 
         public List<StrategyStatistics> GetStatistics()
@@ -58,6 +45,23 @@ namespace Roulette.Core
         public void Clear()
         {
             _strategyResults.Clear();
+        }
+
+        private MapperConfiguration MapperConfiguration()
+        {
+            var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<StrategyResult, StrategyStatistics>()
+                    .ForMember(d => d.Cycles, opt => opt.MapFrom(s => s.CyclesRan))
+                    .ForMember(d => d.Average, opt => opt.MapFrom(s => s.AllBets.Average()))
+                    .ForMember(d => d.Median, opt => opt.MapFrom(s => GetMedian(s.AllBets)))
+                    .ForMember(d => d.EndBalance, opt => opt.MapFrom(s => s.EndBudget - s.StartBudget)));
+            return config;
+        }
+
+        private double GetMedian(List<double> bets)
+        {
+            var orderedBets = bets.OrderBy(b => b).ToList();
+            return orderedBets[(int)(orderedBets.Count / 2)];
         }
     }
 }
